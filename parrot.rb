634c61a915753962c71cab68ce19f7844125757d
Anysia Valko <anysiavalko@gmail.com>
2018-10-23 22:18:05 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(pretty bird = "Squawk!")
-  puts ("pretty bird")
+  puts "pretty bird"
   return "pretty bird"
 end
